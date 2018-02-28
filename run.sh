3de9d05795d3419ceffee068012e105abc7cc653
LukeBoudreau <10604743+LukeBoudreau@users.noreply.github.com>
2018-02-28 04:09:50 +0000

    Added startup script

---
+++ run.sh
@@ -0,0 +1,13 @@
+clear
+echo "[+] Starting Trance Server..."
+PGUSER= \
+PGHOST= \
+PGPASSWORD= \
+PGDATABASE= \
+PGPORT= \
+DPORT= \
+FPATH= \
+TEMP_DIR= \
+STORAGE_IMG_PATH= \
+DEBUG=TRUE \
+node main.js
